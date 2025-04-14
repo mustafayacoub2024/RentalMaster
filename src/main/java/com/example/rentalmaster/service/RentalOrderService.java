21d433e381f4a68dd15ec513aa0ab8bdbb728d53
Mustafa <mustafa.yacoub2013@mail.ru>
2025-04-14 20:29:42 +0300

    RM-0013 добавил методы getRentalOrders и updateRentalStatusById для контролера заявок

--- src/main/java/com/example/rentalmaster/service/RentalOrderService.java
+++ src/main/java/com/example/rentalmaster/service/RentalOrderService.java
@@ -1,7 +1,11 @@
 package com.example.rentalmaster.service;
 
 import com.example.rentalmaster.model.dto.request.RentalOrderRequest;
+import com.example.rentalmaster.model.dto.request.RentalOrderUpdateRequest;
+import com.example.rentalmaster.model.dto.response.RentalOrderGetAllResponse;
 import com.example.rentalmaster.model.dto.response.RentalOrderResponse;
+
+import java.util.List;
 
 public interface RentalOrderService {
     RentalOrderResponse addRentalOrder(RentalOrderRequest rentalOrderRequest);
@@ -9,4 +13,8 @@ public interface RentalOrderService {
     RentalOrderResponse updateStatusByInProgress(String rentalOrderId);
 
     RentalOrderResponse updateStatusByCombleted(String rentalOrderId);
+
+    RentalOrderResponse updateRentalOrder(String rentalOrderId, RentalOrderUpdateRequest rentalOrderRequest);
+
+    List<RentalOrderGetAllResponse> getAllRentalOrders();
 }
